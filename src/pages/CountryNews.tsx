68e3f4a069aeae554a3ba120bd25670671a4566f
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-03-28 13:09:12 +0100

    Add home and country news page

---
+++ src/pages/CountryNews.tsx
@@ -0,0 +1,7 @@
+import News from '../components/news/News';
+
+const CountryNews = () => {
+	return <News />;
+};
+
+export default CountryNews;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Response from '../components/response/Response';
+
+const HomePage = () => {
+	return (
+		<Response
+			text='Choose the country and start reading news!'
+			classes='md:text-lg'
+		/>
+	);
+};
+
+export default HomePage;
