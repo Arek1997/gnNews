3f4292dc7b525334b48123f33fb3205ab450ce1a
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-03-28 13:07:50 +0100

    Add placeholder in case news have not image or description

---
+++ src/data/newsPlaceholder.ts
@@ -0,0 +1,115 @@
+export const newsPlaceholder = [
+	{
+		description:
+			"A modern take on Zenith's long-standing pilot line, complete with El Primero calibers.",
+		urlToImage:
+			'https://hodinkee.imgix.net/uploads/images/29349e77-2ab2-4b39-98c2-1f50240a136f/zenith20230227-216by9.jpg?ixlib=rails-1.1.0&fm=jpg&q=55&auto=format&usm=12&w=1200&h=630&fit=crop',
+	},
+	{
+		description:
+			'The brand returns to the archives after four decades with a Gerald Genta-inspired, throwback design.',
+		urlToImage:
+			'https://hodinkee.imgix.net/uploads/images/074286f6-4027-4524-aed4-f47b9046eecd/L1110352-4.jpg?ixlib=rails-1.1.0&fm=jpg&q=55&auto=format&usm=12&w=1200&h=630&fit=crop',
+	},
+	{
+		description:
+			'Red, blue, and yellow accents make for a more casual Calatrava.',
+		urlToImage:
+			'https://hodinkee.imgix.net/uploads/images/716cfa0f-aac9-47b0-b644-eb60483de6af/16x9forHSite1.png?ixlib=rails-1.1.0&fm=jpg&q=55&auto=format&usm=12&w=1200&h=630&fit=crop',
+	},
+	{
+		description:
+			"The green-bezel Submariner was never an official Kermit. This one is. And it's so much fun.",
+		urlToImage:
+			'https://hodinkee.imgix.net/uploads/images/4d1a4b60-c14a-463d-9750-3ce869611587/20230309_OrisKermit_009.jpg?ixlib=rails-1.1.0&fm=jpg&q=55&auto=format&usm=12&w=1200&h=630&fit=crop',
+	},
+	{
+		description:
+			"Let's all guess which series this is AGE OF OVERLORD -The second core booster of 2023 -Release Date: July 22 2023 -Will have cards for old and new themes -Will have Quarter Century Secret Rares -First…",
+		urlToImage: 'https://r2.ygorganization.com/2018/07/OCG-DM.png',
+	},
+	{
+		description: 'Auto-Haste',
+		urlToImage: 'https://images.pushsquare.com/2251592897dce/1280x720.jpg',
+	},
+	{
+		description:
+			'New Game Pass subscribers can no longer get their first month for a dollar, as Microsoft has stopped offering its introductory Game Pass promotion.',
+		urlToImage:
+			'https://assets-prd.ignimgs.com/2022/11/23/xboxsymbols-blogroll-logos-1646529461207-1669238735793.jpg?width=1280',
+	},
+	{
+		description:
+			'Resident Evil 4 doesn’t mess around. Within the first 10 minutes, poor Leon will be facing off against dozens of villagers at once, which immediately puts st...',
+		urlToImage: 'https://i.ytimg.com/vi/wWLgyDWQdig/maxresdefault.jpg',
+	},
+
+	{
+		description:
+			'One of the best games available for the Nintendo Switch is now being sold for about $1.99 via the Nintendo eShop sale.',
+		urlToImage:
+			'https://www.gizchina.com/wp-content/uploads/images/2023/03/imagem_2023-03-26_205845526.png',
+	},
+	{
+		description:
+			'Dark and Darker has been hit with a DMCA notice by publishers Nexon',
+		urlToImage:
+			'https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/13635db6e0bbcaa2d79ed09ead254745.jpg',
+	},
+	{
+		description:
+			'A Hogwarts Legacy player shows off an interesting discovery that they made in-game while traversing the castle grounds at night.',
+		urlToImage:
+			'https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/03/hogwarts-legacy-interesting-discovery.jpg',
+	},
+	{
+		description:
+			'“As long as your electronic item appears on our ‘Model Name’ list, we will accept it." Walmart wants to pay you for your old phones, tablets, and laptops — regardless of what condition they’re in first appeared on The Cool Down.',
+		urlToImage:
+			'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA196tOg.img?h=315&w=600&m=6&q=60&o=t&l=f&f=jpg',
+	},
+	{
+		description: 'Shaking up what it means to wake up',
+		urlToImage:
+			'https://static1.anpoimages.com/wordpress/wp-content/uploads/2023/03/adaptive-charging-ap-hero.jpg',
+	},
+	{
+		description:
+			'Valve will officially release Counter-Strike 2 in Summer 2023, but here is how you can get access to CS2 limited beta test right now.',
+		urlToImage: 'https://beebom.com/wp-content/uploads/2023/03/CS-2-BETAAA.jpg',
+	},
+	{
+		description:
+			'GOOGLE is warning users to watch out for a pop-up scam that could steal important details and even lead to losing money. The tech giant raised the alarm about the pop-up on its support page. Google…',
+		urlToImage:
+			'https://www.the-sun.com/wp-content/uploads/sites/6/2023/03/MF-GOOGLE-SCAM-OFFPLAT.jpg?strip=all&quality=100&w=1920&h=1080&crop=1',
+	},
+	{
+		description:
+			'Not a chance in hell the skill ships in this state. A Legendary Aspect called "Explosive Mist" will trigger Corpse Explosioin over and over, reset its own co...',
+		urlToImage: 'https://i.ytimg.com/vi/Ll0OPckhsjI/maxresdefault.jpg',
+	},
+	{
+		description:
+			'Intel to increase Xe-Cores on Battlemage GPU series up to 64 According to RedGamingTech, Intel’s next-gen GPU could see a doubled core count. As a reminder, RedGamingTech had already leaked a roadmap featuring Intel Arc GPU plans for the near future. The pres…',
+		urlToImage: 'https://cdn.videocardz.com/1/2023/03/INTEL-ARC-GPU-HERO-3.jpg',
+	},
+	{
+		description:
+			'At the time of writing this, the price of the PS5 console which comes with God of War Ragnarök was selling for just £499.99.',
+		urlToImage:
+			'https://www.gizchina.com/wp-content/uploads/images/2023/03/6523224_sd-1200x608.jpg',
+	},
+	{
+		description: 'Melee this idea to rest',
+		urlToImage:
+			'https://gonintendo.com/attachments/image/25523/file/medium-b8b7c13de9dd1448fd650974cec98c48.png',
+	},
+	{
+		description:
+			'Infinix HOT 30i has a premium diamond pattern design 50 megapixels camera and a 5000mAh battery It will go on sale on April 3rd at a special price of Rs 8999',
+
+		urlToImage:
+			'https://imgeng.jagran.com/images/2023/mar/infinix-hot-30i-launched-in-india1679900368173.jpg',
+	},
+];
